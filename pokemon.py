14641107bebdc9618577768288b5431851291dca
Icaroyuri <ycaroyuri@gmail.com>
2026-08-25 17:02:11 -0300

    Pokemon2

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,13 @@
 import requests
 
 pokemon = input("Diga o nome do seu pokemon para saber o tipo primário: ")
+try:
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    dados = requests.get(url).json()
 
-url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
-dados = requests.get(url).json()
+    print(dados['types'][0]['type']['name'])
+    print(dados['types'][1]['type']['name'])
 
-print(dados['types'][0]['type']['name'])
+except:
+    print("Pokemon não encontrado")
 
